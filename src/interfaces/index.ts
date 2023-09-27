5b15446ad4ac3e8a3a7b9e5eed8087844cbf2f90
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-27 00:39:48 -0300

    feat: coffees map

---
+++ src/interfaces/index.ts
@@ -0,0 +1,8 @@
+export interface Coffee {
+  id: number;
+  name: string;
+  description: string;
+  type: string[];
+  price: number;
+  highlight: string;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,26 @@
-import { Introduction } from '../../components/Introduction';
 import * as S from './styles';
+import { Introduction } from '../../components/Introduction';
+import { Coffee } from '../../interfaces';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
 
 export const Home = () => {
+  const [coffees, setCoffees] = useState<Coffee[]>();
+
+  useEffect(() => {
+    api.get('/coffees').then(response => {
+      setCoffees(response.data);
+    })
+  }, []);
+
   return (
     <S.HomeContainer>
       <Introduction />
+      {coffees?.map((coffee) => (
+        <div key={coffee.id}>
+          <img src={coffee.highlight} alt="" />
+        </div>
+      ))}
     </S.HomeContainer>
   )
 }
